fcaf77d1fb2e108decffa875a9f99a326174c087
Huan Lu <huanlu@huans-mbp.attlocal.net>
2017-10-08 20:37:15 -0700

    create project

---
+++ LC-OC/LC-OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LC-OC
+//
+//  Created by Huan Lu on 10/8/17.
+//  Copyright © 2017 Huan Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
