2fb5974ddaa0cac729791f081322beb2e3a2b186
Huan Lu <huanlu@huans-mbp.attlocal.net>
2017-10-11 00:20:19 -0700

    add implementation of thirdMax

--- LC-OC/LC-OC/ThirdMax.m
+++ LC-OC/LC-OC/ThirdMax.m
@@ -11,7 +11,21 @@
 @implementation ThirdMax
 
 + (NSInteger)thirdMaxFromArray:(NSArray *)nums {
-    return 0;
+    long long max1 = LLONG_MIN, max2 = LLONG_MIN, max3 = LLONG_MIN;
+    for (int i = 0; i < [nums count]; i++) {
+        int num = [nums[i] intValue];
+        if (num > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = num;
+        } else if (num < max1 && num > max2) {
+            max3 = max2;
+            max2 = num;
+        } else if (num < max2 && num > max3) {
+            max3 = num;
+        }
+    }
+    return max3 == LLONG_MIN ? max1 : max3;
 }
 
 @end
