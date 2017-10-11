d74f6301e3457badf6183d85b519c6092933a9af
Huan Lu <huanlu@huans-mbp.attlocal.net>
2017-10-10 23:54:48 -0700

    add declaration of thirdMax

---
+++ LC-OC/LC-OC/ThirdMax.h
@@ -0,0 +1,15 @@
+//
+//  ThirdMax.h
+//  LC-OC
+//
+//  Created by Huan Lu on 10/10/17.
+//  Copyright © 2017 Huan Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThirdMax : NSObject
+
++ (NSInteger)thirdMaxFromArray:(NSArray *)nums;
+
+@end
---
+++ LC-OC/LC-OC/ThirdMax.m
@@ -0,0 +1,17 @@
+//
+//  ThirdMax.m
+//  LC-OC
+//
+//  Created by Huan Lu on 10/10/17.
+//  Copyright © 2017 Huan Lu. All rights reserved.
+//
+
+#import "ThirdMax.h"
+
+@implementation ThirdMax
+
++ (NSInteger)thirdMaxFromArray:(NSArray *)nums {
+    return 0;
+}
+
+@end
