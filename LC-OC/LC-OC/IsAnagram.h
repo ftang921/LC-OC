999662b309c647aa0fc37c3cb10e0826d66635e6
Huan Lu <huanlu@huans-mbp.attlocal.net>
2017-10-10 23:50:48 -0700

    add declaration of IsAnagram

---
+++ LC-OC/LC-OC/IsAnagram.h
@@ -0,0 +1,15 @@
+//
+//  IsAnagram.h
+//  LC-OC
+//
+//  Created by Huan Lu on 10/10/17.
+//  Copyright © 2017 Huan Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IsAnagram : NSObject
+
++ (BOOL)isAnagramForString:(NSString *)s andString:(NSString *)p;
+
+@end
---
+++ LC-OC/LC-OC/IsAnagram.m
@@ -0,0 +1,17 @@
+//
+//  IsAnagram.m
+//  LC-OC
+//
+//  Created by Huan Lu on 10/10/17.
+//  Copyright © 2017 Huan Lu. All rights reserved.
+//
+
+#import "IsAnagram.h"
+
+@implementation IsAnagram
+
++ (BOOL)isAnagramForString:(NSString *)s andString:(NSString *)p {
+    return NO;
+}
+
+@end
