e7fd7c6764d1ccff3ea31f202304cd4cee7c1032
Huan Lu <huanlu@huans-mbp.attlocal.net>
2017-10-10 23:56:55 -0700

    add firstUniqChar

---
+++ LC-OC/LC-OC/FirstUniqChar.h
@@ -0,0 +1,15 @@
+//
+//  FirstUniqChar.h
+//  LC-OC
+//
+//  Created by Huan Lu on 10/10/17.
+//  Copyright © 2017 Huan Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstUniqChar : NSObject
+
++ (int)firstUniqCharFromString:(NSString *)s;
+
+@end
---
+++ LC-OC/LC-OC/FirstUniqChar.m
@@ -0,0 +1,17 @@
+//
+//  FirstUniqChar.m
+//  LC-OC
+//
+//  Created by Huan Lu on 10/10/17.
+//  Copyright © 2017 Huan Lu. All rights reserved.
+//
+
+#import "FirstUniqChar.h"
+
+@implementation FirstUniqChar
+
++ (int)firstUniqCharFromString:(NSString *)s {
+    return -1;
+}
+
+@end
