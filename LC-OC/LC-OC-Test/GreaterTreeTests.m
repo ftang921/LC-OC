ee0fb19b5da35bb6a10e27eb110adb292fb571c3
Feng Tang <arwintang@gmail.com>
2017-10-11 22:30:20 -0700

    Greater tree tests

---
+++ LC-OC/LC-OC-Test/GreaterTreeTests.m
@@ -0,0 +1,34 @@
+//
+//  GreaterTreeTests.m
+//  LC-OC-Test
+//
+//  Created by Feng Tang on 10/11/17.
+//  Copyright © 2017 Huan Lu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "GreaterTree.h"
+
+@interface GreaterTreeTests : XCTestCase
+
+@end
+
+@implementation GreaterTreeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testGreaterTree {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
