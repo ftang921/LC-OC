39130c1a81cf67a2be9425b4c2e74a578d5f1abf
Feng Tang <arwintang@gmail.com>
2017-10-11 23:16:39 -0700

    Add greater tree test

--- LC-OC/LC-OC-Test/GreaterTreeTests.m
+++ LC-OC/LC-OC-Test/GreaterTreeTests.m
@@ -9,16 +9,30 @@
 #import <XCTest/XCTest.h>
 
 #import "GreaterTree.h"
+#import "TreeNode.h"
 
 @interface GreaterTreeTests : XCTestCase
+
+@property(nonatomic, readwrite, strong) TreeNode *testRoot;
+@property(nonatomic, readwrite, strong) TreeNode *expectedRoot;
 
 @end
 
 @implementation GreaterTreeTests
 
 - (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+  [super setUp];
+  self.testRoot = [TreeNode TreeNodeWithVal:2 left:nil right:nil];
+  self.testRoot.left = [TreeNode TreeNodeWithVal:0 left:nil right:nil];
+  self.testRoot.right = [TreeNode TreeNodeWithVal:3 left:nil right:nil];
+  self.testRoot.left.left = [TreeNode TreeNodeWithVal:-4 left:nil right:nil];
+  self.testRoot.left.right = [TreeNode TreeNodeWithVal:1 left:nil right:nil];
+  
+  self.expectedRoot = [TreeNode TreeNodeWithVal:5 left:nil right:nil];
+  self.expectedRoot.left = [TreeNode TreeNodeWithVal:6 left:nil right:nil];
+  self.expectedRoot.right = [TreeNode TreeNodeWithVal:3 left:nil right:nil];
+  self.expectedRoot.left.left = [TreeNode TreeNodeWithVal:2 left:nil right:nil];
+  self.expectedRoot.left.right = [TreeNode TreeNodeWithVal:6 left:nil right:nil];
 }
 
 - (void)tearDown {
@@ -27,8 +41,8 @@
 }
 
 - (void)testGreaterTree {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  TreeNode *resultRoot = [GreaterTree greaterBSTFromBST:self.testRoot];
+  XCTAssertEqualObjects(resultRoot, self.expectedRoot);
 }
 
 @end
