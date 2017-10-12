732adba68e2bc0bbe1d0ed76036de1cbf9d04612
Huan Lu <huanlu@huans-mbp.attlocal.net>
2017-10-11 23:35:41 -0700

    add implementation of 3sum

--- LC-OC/LC-OC/ThreeSum.m
+++ LC-OC/LC-OC/ThreeSum.m
@@ -12,10 +12,34 @@
 
 + (NSArray *)threeSum:(NSArray *)nums {
     // sort
-    
+    NSArray *sortedArray = [nums sortedArrayUsingComparator:^NSComparisonResult(NSNumber *num1, NSNumber *num2) {
+        return num1 < num2;
+    }];
     
     // let i be the index of the first number, use in subarray nums[i+1:]
     // find two numbers that sum up to -nums[i]
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (int first = 0; first < (int)[sortedArray count]; first++) {
+        int firstNumber = [(NSNumber *)sortedArray[first] intValue];
+        int second = first + 1, third = (int)[sortedArray count] - 1;
+        if (first > 0 && firstNumber == [(NSNumber *)sortedArray[first - 1] intValue]) {
+            continue;
+        }
+        while (second < third) {
+            int secondNumber = [(NSNumber *)sortedArray[second] intValue];
+            int thirdNumber = [(NSNumber *)sortedArray[third] intValue];
+            if (second > first + 1 && secondNumber == [(NSNumber *)sortedArray[second - 1] intValue]) {
+                second++;
+            } else if (firstNumber + secondNumber + thirdNumber == 0) {
+                NSArray *solution = @[@(first), @(second), @(third)];
+                [result addObject:solution];
+            } else if (firstNumber + secondNumber + thirdNumber < 0) {
+                second++;
+            } else {
+                third--;
+            }
+        }
+    }
     return nil;
 }
 
